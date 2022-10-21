590a7c2eb3dd79b1cc7b921f577103a90da45e2e
favourloverboy <favourehio2019@gmail.com>
2022-10-21 02:43:34 +0100

    adding-members

--- middleware/check.js
+++ middleware/check.js
@@ -11,8 +11,14 @@ const findByHomeCellName = async (name) => {
     const result = await db.promise().query(sql, name);
     return result[0][0];
 }
+const findByMemberEmail = async (email) => {
+    let sql = 'SELECT * FROM members WHERE email = ?';
+    const result = await db.promise().query(sql, email);
+    return result[0][0];
+}
 
 module.exports = {
     findByChurchName,
-    findByHomeCellName
+    findByHomeCellName,
+    findByMemberEmail
 }
