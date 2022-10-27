6aedb59a37f5cd04e7373c240ea7516aaa995aed
favourloverboy <favourehio2019@gmail.com>
2022-10-27 18:28:52 +0100

    assign homecell leaders by church leaders

--- routes/churchRoutes.js
+++ routes/churchRoutes.js
@@ -16,6 +16,13 @@ router.get(`/${church}/dashboard`, checkLogin.checkAuthenticated, controllingUse
 router.get(`/${church}/homecell`, checkLogin.checkAuthenticated, controllingUserAccess.userAccess, (authController.homecell));
 
 router.get(`/${church}/add_homecell`, checkLogin.checkAuthenticated, controllingUserAccess.userAccess, (authController.add_homecell));
+router.post(`/${church}/add_homecell_leader`, checkLogin.checkAuthenticated, controllingUserAccess.userAccess, [
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email can\'t be empty')
+        .bail()
+        .toLowerCase()
+], (authController.add_homecell_leader_post));
 
 router.get(`/${church}/add_homecell_leader/:id`, checkLogin.checkAuthenticated, controllingUserAccess.userAccess, (authController.add_homecell_leader));
 
